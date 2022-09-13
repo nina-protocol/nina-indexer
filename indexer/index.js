c471892c022a41cfbedb900781d63ba2b8d8e3d9
facing-n <mike@nina.market>
2022-09-13 12:30:57 -0400

    add env var RUN_INITIAL_SYNC

--- indexer/index.js
+++ indexer/index.js
@@ -7,14 +7,15 @@ const NinaProcessor = require('./processor');
 const startProcessing = async () => {
   console.log('Indexer Starting Up')
   await initDb()
-
   await NinaProcessor.init()
   console.log('Indexer Started - DB and Processor Initialized')
-  
-  console.log('Initial Sync starting')
-  await NinaProcessor.runDbProcesses()
-  await NinaProcessor.processCollectors()
-  console.log('Initial Sync complete')
+
+  if (process.env.RUN_INITAL_SYNC) {
+    console.log('Initial Sync starting')
+    await NinaProcessor.runDbProcesses()
+    await NinaProcessor.processCollectors()
+    console.log('Initial Sync complete')
+  }
 
   cron.schedule('* * * * *', async() => {
     console.log('Cron job starting: Sync Hubs + Releases');
