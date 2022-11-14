b1d05b0e3b8190b4429986a7db08a01f6434f38f
Eric Farber <eric.j.farber@gmail.com>
2022-11-14 16:29:48 -0500

    log

--- api/api.js
+++ api/api.js
@@ -805,7 +805,9 @@ module.exports = (router) => {
         const hubCollaborator = await lookupCollaborator(ctx.params.hubCollaboratorPublicKey)
         if (hubCollaborator) {
           console.log('hubCollaborator :>> ', hubCollaborator);
+          console.log('hub id', hub.id)
           const collaborator = await Account.findOrCreate(hubCollaborator.collaborator.toBase58())
+          console.log('collaborator', collaborator)
           await Hub.relatedQuery('collaborators').for(hub.id).relate({
             id: collaborator.id,
             hubCollaboratorPublicKey: ctx.params.hubCollaboratorPublicKey,
