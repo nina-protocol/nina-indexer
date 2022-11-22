da29dcd922b5a0cc82da0f0c801bab216e4d0ab0
facing-n <mike@nina.market>
2022-11-22 15:44:37 -0500

    .

--- indexer/processor.js
+++ indexer/processor.js
@@ -111,7 +111,7 @@ class NinaProcessor {
       publicKey: publicKey.toBase58(),
     }
     const { registry } = await NameRegistryState.retrieve(this.provider.connection, publicKey)
-    console.log('registry', registry, publicKey)
+    console.log('registry', registry, publicKey.toBase58())
     if (registry.parentName.toBase58() === NINA_ID_ETH_TLD.toBase58()) {
       const nameAccountKey = await getNameAccountKey(await getHashedName(registry.owner.toBase58()), NINA_ID, NINA_ID_ETH_TLD);
       const name = await ReverseEthAddressRegistryState.retrieve(this.provider.connection, nameAccountKey)
