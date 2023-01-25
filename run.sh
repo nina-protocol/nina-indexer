fc01fa87b33fe1c3a67b568989efc852bacba000
Brandon Nickell <bbnickell@gmail.com>
2023-01-25 16:01:10 -0600

    pm2 stop/start added

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/ec2-user/nina-indexer
-yarn restart:indexer
+pm2 stop nina-indexer && pm2 delete nina-indexer
+pm2 start yarn --interpreter bash --name nina-indexer -- start:indexer
