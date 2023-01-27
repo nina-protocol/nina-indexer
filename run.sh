482d946c35dfa3ea2686d917b297f218a99455b0
Brandon Nickell <bbnickell@gmail.com>
2023-01-27 15:04:47 -0600

    Full path to pm2

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/nina-indexer
-pm2 stop nina-indexer && pm2 delete nina-indexer
-pm2 start yarn --interpreter bash --name nina-indexer -- start:indexer
-pm2 stop nina-api && pm2 delete nina-api
-pm2 start yarn --interpreter bash --name nina-api -- start:api
+~/.nvm/versions/node/v16.17.0/bin/pm2 stop nina-indexer && pm2 delete nina-indexer
+~/.nvm/versions/node/v16.17.0/bin/pm2 start yarn --interpreter bash --name nina-indexer -- start:indexer
+~/.nvm/versions/node/v16.17.0/bin/pm2 stop nina-api && pm2 delete nina-api
+~/.nvm/versions/node/v16.17.0/bin/pm2 start yarn --interpreter bash --name nina-api -- start:api
