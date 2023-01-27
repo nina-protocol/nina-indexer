45125e68de84f028c4f925a34a97f5886ee4853b
Brandon Nickell <bbnickell@gmail.com>
2023-01-27 15:08:49 -0600

    /Users/nickell/.nvm/versions/node/v16.4.1/bin/pm2

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/nina-indexer
 $NVM_BIN/pm2 stop nina-indexer && pm2 delete nina-indexer
-$NVM_BIN/v16.17.0/bin/pm2 start yarn --interpreter bash --name nina-indexer -- start:indexer
-$NVM_BIN/bin/pm2 stop nina-api && pm2 delete nina-api
+$NVM_BIN/pm2 start yarn --interpreter bash --name nina-indexer -- start:indexer
+$NVM_BIN/pm2 stop nina-api && pm2 delete nina-api
 $NVM_BIN/pm2 start yarn --interpreter bash --name nina-api -- start:api
