7b2b61832c1fc6cf20b2f7ae21ded6de481bfa0d
Brandon Nickell <bbnickell@gmail.com>
2023-01-30 14:08:09 -0600

    Absolute path for pm2

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user/nina-indexer
-$NVM_BIN/pm2 stop nina-indexer && pm2 delete nina-indexer
-$NVM_BIN/pm2 start yarn --interpreter bash --name nina-indexer -- start:indexer
-$NVM_BIN/pm2 stop nina-api && pm2 delete nina-api
-$NVM_BIN/pm2 start yarn --interpreter bash --name nina-api -- start:api
-$NVM_BIN/pm2 status
+/home/ec2-user/.nvm/versions/node/v16.17.0/bin/pm2 stop nina-indexer && pm2 delete nina-indexer
+/home/ec2-user/.nvm/versions/node/v16.17.0/bin/pm2 start yarn --interpreter bash --name nina-indexer -- start:indexer
+/home/ec2-user/.nvm/versions/node/v16.17.0/bin/pm2 stop nina-api && pm2 delete nina-api
+/home/ec2-user/.nvm/versions/node/v16.17.0/bin/pm2 start yarn --interpreter bash --name nina-api -- start:api
+/home/ec2-user/.nvm/versions/node/v16.17.0/bin/pm2 status
