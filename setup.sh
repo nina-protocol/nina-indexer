22db47e05aafd66b4dace7ab4c4c27a86eaebed4
Brandon Nickell <bbnickell@gmail.com>
2023-01-30 21:18:52 -0600

    Add NVM to setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 sudo service codedeploy-agent status
 chmod +x /home/ec2-user/nina-indexer/run.sh
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
