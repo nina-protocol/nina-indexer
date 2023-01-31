61f9c453c3733d071732a1978d963035273d3dcf
Brandon Nickell <bbnickell@gmail.com>
2023-01-31 13:00:50 -0600

    Update appspec

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 cd /home/ec2-user/nina-indexer
 nvm version
 pm2 stop nina-indexer && pm2 delete nina-indexer
