a082ed7fb1f0994d937b238fae3897394a53561c
Brandon Nickell <bbnickell@gmail.com>
2023-01-31 13:57:16 -0600

    Remove stop.sh

--- stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-pm2 stop nina-indexer && pm2 delete nina-indexer
-pm2 stop nina-api && pm2 delete nina-api
