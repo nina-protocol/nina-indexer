97d77a3af3406d5dcb70c243c79b9a4ddc089375
Brandon Nickell <bbnickell@gmail.com>
2023-01-30 21:22:04 -0600

    Add nvm version

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/nina-indexer
+nvm version
 pm2 stop nina-indexer && pm2 delete nina-indexer
 pm2 start yarn --interpreter bash --name nina-indexer -- start:indexer
 pm2 stop nina-api && pm2 delete nina-api
