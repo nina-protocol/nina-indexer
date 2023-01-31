5d13ad6c96696ecb42d8ada3226c7cfe9343fa45
Brandon Nickell <bbnickell@gmail.com>
2023-01-31 13:47:16 -0600

    Replace ApplicationStop with BeforeInstall

--- stop.sh
+++ stop.sh
@@ -3,6 +3,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-cd /home/ec2-user/nina-indexer
 pm2 stop nina-indexer && pm2 delete nina-indexer
 pm2 stop nina-api && pm2 delete nina-api
