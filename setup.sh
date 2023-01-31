81d2f6e8fafcf99e75eeef9740c30c3c3afddd06
Brandon Nickell <bbnickell@gmail.com>
2023-01-31 14:22:13 -0600

    npm install

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ chmod +x /home/ec2-user/nina-indexer/run.sh
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+npm install
