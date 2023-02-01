fc4ae41c4603dd64cbb0341b7d500dc3da4b4412
Brandon Nickell <bbnickell@gmail.com>
2023-01-31 20:18:30 -0600

    Remove --interpreter bash

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 cd /home/ec2-user/nina-indexer
-pm2 start yarn --interpreter bash\ --name nina-indexer\ -- start:indexer
-pm2 start yarn --interpreter bash\ --name nina-api\ -- start:api
+pm2 start yarn --name nina-indexer -- start:indexer
+pm2 start yarn --name nina-api -- start:api
