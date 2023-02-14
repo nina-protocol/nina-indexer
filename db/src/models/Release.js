dfa0c993d7f43c9c2671a74cf4ed20fc0748ba4a
facing-n <mike@nina.market>
2023-02-13 19:14:02 -0500

    turn tweetbot back on + turn ig verfication check on

--- db/src/models/Release.js
+++ db/src/models/Release.js
@@ -78,7 +78,7 @@ export default class Release extends Model {
       publisherId,
     })
     await this.processRevenueShares(releaseAccount, release);
-    // tweetNewRelease(metadata);
+    tweetNewRelease(metadata);
     console.log('Inserted Release: ', publicKey)
     return release;
   }
