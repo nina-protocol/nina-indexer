2d257438f94943edcfaa972e827ba2466fbe67c8
Brandon Nickell <bbnickell@gmail.com>
2023-03-02 15:51:31 -0600

    Remove source command

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-source /home/ec2-user/.env.development
 cd /home/ec2-user/nina-indexer
 pm2 start yarn --name nina-indexer -- start:indexer
 pm2 start yarn --name nina-api -- start:api
