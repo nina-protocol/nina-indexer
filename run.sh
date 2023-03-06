c3fc6d8e542bb2dd9b8ae8d769301b0d53a70fa8
Brandon Nickell <bbnickell@gmail.com>
2023-03-06 17:30:50 -0600

    hostname detection for env

--- run.sh
+++ run.sh
@@ -3,6 +3,12 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+if [ "$HOSTNAME" = "dev.api.ninaprotocol.com" ]; then
+    source /home/ec2-user/.env.development
+else
+    source /home/ec2-user/.env.production
+fi
+
 cd /home/ec2-user/nina-indexer
 pm2 start yarn --name nina-indexer -- start:indexer
 pm2 start yarn --name nina-api -- start:api
