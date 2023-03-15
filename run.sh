152cca8ac423126e7e517a641214231eebf05c4c
Brandon Nickell <bbnickell@gmail.com>
2023-03-15 13:31:59 -0500

    Update run and setup scripts

--- run.sh
+++ run.sh
@@ -3,9 +3,14 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+echo "Running as `whoami` user"
+source ~/.bashrc
+
 if [ "$HOSTNAME" = "dev.api.ninaprotocol.com" ]; then
     source /home/ec2-user/.env.development
+    echo "dev environment detected for host: $HOSTNAME"
 else
+    echo "non-dev environment detected for host: $HOSTNAME"
     source /home/ec2-user/.env.production
 fi
 
