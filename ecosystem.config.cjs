9fb079449023039ccbb0be772752c335a0994424
Brandon Nickell <bbnickell@gmail.com>
2023-04-26 16:21:29 -0500

    Add Discord PM2 webhook URL

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,11 +5,23 @@ module.exports = {
       script: 'indexer/index.js',
       max_memory_restart: '1536M',
       args: '-- start:indexer:heapstats',
+      pm2_discord: {
+        url: process.env.DISCORD_WEBHOOK_URL,
+        events: {
+          restart: true,
+        },
+      },
     },
     {
       name: 'nina-api',
       script: 'api/index.js',
       max_memory_restart: '1536M',
+      pm2_discord: {
+        url: process.env.DISCORD_WEBHOOK_URL,
+        events: {
+          restart: true,
+        },
+      },
     },
   ],
 };
