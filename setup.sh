b3354209223e50c7d9e8ef367911dae35d1815d6
Brandon Nickell <bbnickell@gmail.com>
2023-11-12 22:31:43 -0500

    chmod command

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 set -ex
 
 sudo chown -R ec2-user /home/ec2-user/nina-indexer
-sudo chmod +x /home/ec2-user/nina-indexer/run.sh
+chmod +x /home/ec2-user/nina-indexer/run.sh
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
