88c90f2425431bed6049af92e12a904074d1ebc1
facing-n <mike@ninaprotocol.com>
2024-04-22 11:46:36 -0400

    park

---
+++ db/src/migrations/20240321155034_hide_release.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = function(knex) {
+  return knex.schema.table('releases' , table => {
+    table.boolean('hidden').notNullable().defaultTo(false);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = function(knex) {
+  
+};
