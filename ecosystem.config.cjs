c5e973df4d51f01cf8ae5b93e8c0d958bd21a85f
Brandon Nickell <bbnickell@gmail.com>
2024-09-12 13:11:45 -0500

    Iniitial Indexer v2 commit

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,20 +2,11 @@ module.exports = {
   apps: [
     {
       name: 'nina-indexer',
-      script: 'indexer/index.js',
-      max_memory_restart: '1536M',
-      args: '-- start:indexer:heapstats',
-      env: {
-        // Will be temporarily written to during pm2 env configuration
-      }
+      script: 'indexer/index.js'
     },
     {
       name: 'nina-api',
-      script: 'api/index.js',
-      max_memory_restart: '1536M',
-      env: {
-        // Will be temporarily written to during pm2 env configuration
-      }
+      script: 'api/index.js'
     },
   ],
 };
