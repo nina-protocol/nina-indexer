76914c6553112143be47a7ecc114ce9deb988228
facing-n <mike@ninaprotocol.com>
2024-10-14 18:31:09 -0400

    sort tags by count

--- api/api.js
+++ api/api.js
@@ -2737,18 +2737,29 @@ export default (router) => {
   router.get('/tags', async (ctx) => {
     try {
       let { offset=0, limit=20, sort='desc', query='' } = ctx.query;
-      const tags = await Tag.query()
-        .where('value', 'ilike', `%${query}%`)
-        .orderBy('value', sort)
-        .range(Number(offset), Number(offset) + Number(limit) - 1);
-
-      for await (let tag of tags.results) {
-        // const count = await Tag.relatedQuery('releases').for(tag.id).resultSize();
-        tag.count = await Tag.relatedQuery('releases').for(tag.id).resultSize();
-        await tag.format();
+      
+      const tags = await db.raw(`
+        SELECT tags.*, COUNT(tags_releases."tagId") as count
+        FROM tags
+        JOIN tags_releases ON tags.id = tags_releases."tagId"
+        WHERE tags.value ILIKE '%${query}%'
+        GROUP BY tags.id
+        ORDER BY count ${sort}
+        LIMIT ${limit}
+        OFFSET ${offset}
+      `)
+        
+      const total = await Tag.query().where('value', 'ilike', `%${query}%`).resultSize()
+    
+      for await (let tag of tags.rows) {
+        tag.count = Number(tag.count)
+        delete tag.id
       }
       ctx.body = {
-        tags
+        tags: {
+          results: tags.rows,
+          total,
+        }
       }
     } catch (error) {
       console.warn(error)
