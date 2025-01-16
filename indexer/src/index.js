6301875c2e657a9593af0a4ed5a4453884bcc143
facing-n <mike@ninaprotocol.com>
2025-01-16 10:52:35 -0500

    release syncer restricted release handling

--- indexer/src/index.js
+++ indexer/src/index.js
@@ -8,6 +8,7 @@ import { initDb, config } from '@nina-protocol/nina-db';
 import TransactionSyncer from './TransactionSyncer.js';
 import VerificationSyncer from './VerificationSyncer.js';
 import CollectorSyncer from './CollectorSyncer.js';
+import ReleaseSyncer from './ReleaseSyncer.js';
 
 function getUsedHeapSize() {
     const heapStats = v8.getHeapStatistics();
@@ -36,13 +37,15 @@ const startProcessing = async () => {
     logTimestampedMessage('initDb completed.');
     await TransactionSyncer.initialize();
     await CollectorSyncer.initialize();
+    await ReleaseSyncer.syncReleases();
 
     await TransactionSyncer.syncTransactions(); // initial sync
 
     cron.schedule('* * * * *', async() => {
         logTimestampedMessage(`Starting scheduled transaction sync`);
         await TransactionSyncer.syncTransactions();
-
+        await ReleaseSyncer.syncReleases();
+        
         if (process.argv[2] === "--heap-stats") {
             runHeapDiagnostics(); // verbose heap diagnostics if option enabled
         }
