356173caaa7350065991dfa2abd12a80ef4f74bf
facing-n <mike@ninaprotocol.com>
2025-12-16 14:48:14 -0500

    db migration for posts archive

---
+++ db/src/migrations/20251216000000_add_posts_archived.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = function(knex) {
+  return knex.schema.table('posts', table => {
+      table.boolean('archived').defaultTo(false);
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = function(knex) {
+  return knex.schema.table('posts', table => {
+      table.dropColumn('archived');
+    });
+};
