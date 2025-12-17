2076c20610caaa1b7f0abc0cfcce6a18df415eb8
facing-n <mike@ninaprotocol.com>
2025-12-17 11:44:58 -0500

    remove awaits from db subquerys

--- api/routes/PostRouter.js
+++ api/routes/PostRouter.js
@@ -23,7 +23,7 @@ router.get('/', async (ctx) => {
     const posts = await Post
     .query()
     .where('archived', false)
-    .where(async (qb) => {
+    .where((qb) => {
       qb
         .where(ref('data:title').castText(), 'ilike', `%${query}%`)
         .orWhere(ref('data:description').castText(), 'ilike', `%${query}%`)
