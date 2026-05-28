18970672f11f0da84886cb4d32437f67a83d36da
facing-n <mike@ninaprotocol.com>
2026-05-28 13:53:02 -0400

    .

--- db/src/migrations/20260511000000_add_deleted_at_to_accounts.js
+++ db/src/migrations/20260511000000_add_deleted_at_to_accounts.js
@@ -1,9 +1,20 @@
-exports.up = (knex) =>
-  knex.schema.alterTable('accounts', (table) => {
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = function(knex) {
+  return knex.schema.alterTable('accounts', (table) => {
     table.timestamp('deleted_at').nullable();
   });
 
-exports.down = (knex) =>
-  knex.schema.alterTable('accounts', (table) => {
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = function(knex) {
+  return knex.schema.alterTable('accounts', (table) => {
     table.dropColumn('deleted_at');
   });
+};
